d9782885096c81019bf0bb0591b2531c3771bc23
chimezie <g00387847@gmail.ie>
2020-02-29 10:07:22 +0000

    modified BMI

--- BMI.py
+++ BMI.py
@@ -1,3 +1,7 @@
+# Bonny Nwosu
+# This program  calculate somebody's Body Mass Index (BMI)
+# Using height in centimetres and weight in kilograms
+  
 weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in cm: "))
 
@@ -6,3 +10,4 @@ height_in_meters = height / 100
 bmi = round(weight / height_in_meters**2, 2)
 
 print(bmi)
+
