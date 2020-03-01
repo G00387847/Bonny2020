135af9c995b1532c4ceee9a7006534881b973f46
chimezie <g00387847@gmail.ie>
2020-03-01 13:24:13 +0000

    commit file called square_root

---
+++ Square_root.py
@@ -0,0 +1,11 @@
+# Bonny Chimezie Nwosu
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 1
+
+# To take the input from the user
+num = float(input('Please enter a positve number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.1f is approx. %0.1f.'%(num ,num_sqrt))
