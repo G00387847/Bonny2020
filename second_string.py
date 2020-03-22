36fb26a1a13c5e4e2b25d311392e17c65bc88ee0
chimezie <g00387847@gmail.ie>
2020-03-22 11:16:57 +0000

    modify file

--- second_string.py
+++ second_string.py
@@ -1,8 +1,7 @@
 # Bonny Nwosu
-# This program takes asks a user to input a strinh
+# This program takes asks a user to input a string
 # And output every second letter in reverse order.
 # Using Loop
-<<<<<<< HEAD
 
 num1 = input("Please enter a sentence")
 
@@ -13,18 +12,3 @@ def reverse(num1):
   return str
 print(end="")
 print(num1[::-2])
-=======
-
-num1 = input("Please enter a sentence")
-
-def reverse(num1): 
-  str = "" 
-  for i in num1: 
-    str = i + str
-  return str
-print(end="")
-print(num1[::-2])
-
-
-  
->>>>>>> dd91a9d0ab2155316c5e3074111046515ba67581
